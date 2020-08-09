922dfa8cc029595af90f08b8064ed877a73da704
Tom Ramsden <ramsdentom@gmail.com>
2020-08-09 15:27:10 +0100

    Implement creating a project

--- router.go
+++ router.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -37,10 +38,28 @@ func getProject(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(project)
 }
 
+func createProject(w http.ResponseWriter, r *http.Request) {
+	var project project
+
+	request, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Error reading Project from request: %v", err)
+	}
+
+	json.Unmarshal(request, &project)
+
+	database.Create(project)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(project)
+}
+
 func createRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", index).Methods("GET")
+	router.HandleFunc("/project", createProject).Methods("POST")
 	router.HandleFunc("/projects", getProjects).Methods("GET")
 	router.HandleFunc("/project/{id}", getProject).Methods("GET")
 
