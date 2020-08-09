18f3213065826104dd93d7f010102f0b72a7860d
Tom Ramsden <ramsdentom@gmail.com>
2020-08-10 00:53:10 +0100

    Move config/database initialisation into own functions

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,37 +13,56 @@ import (
 var database kibisis.Database
 var config ApplicationConfig
 
-func main() {
+func initialiseConfig() error {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(".")
 	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatalf("Error loading config file: %v", err)
+		return fmt.Errorf("Error loading config file: %v", err)
 	}
 
 	err = viper.UnmarshalKey("aws", &config)
 	if err != nil {
-		log.Fatalf("Error reading config file: %v", err)
+		return fmt.Errorf("Error reading config file: %v", err)
 	}
 
 	err = validate(config)
 	if err != nil {
-		log.Fatalf("Configuration file error: %v", err)
+		return fmt.Errorf("Configuration file error: %v", err)
 	}
 
+	return nil
+}
+
+func initialiseDatabase() error {
+	var err error
 	database, err = kibisis.GetDriver("arangoDB")
 	if err != nil {
-		log.Fatalf("Error loading database driver: %v", err)
+		return fmt.Errorf("Error loading database driver: %v", err)
 	}
 
 	err = database.Conn()
 	if err != nil {
-		log.Fatalf("Error connecting to database: %v", err)
+		return fmt.Errorf("Error connecting to database: %v", err)
 	}
 
 	err = database.Init("venturelist", "projects")
 	if err != nil {
-		log.Fatalf("Error initialising database: %v", err)
+		return fmt.Errorf("Error initialising database: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	err := initialiseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = initialiseDatabase()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	router := createRouter()
