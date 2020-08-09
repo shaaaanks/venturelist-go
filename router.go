733a6bc78f79875fd16b419a4e900166ba19f76b
Tom Ramsden <ramsdentom@gmail.com>
2020-08-09 15:21:31 +0100

    Implement getting single project

--- router.go
+++ router.go
@@ -25,11 +25,24 @@ func getProjects(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(projects)
 }
 
+func getProject(w http.ResponseWriter, r *http.Request) {
+	projectID := mux.Vars(r)["id"]
+
+	project, err := database.Find(projectID)
+	if err != nil {
+		log.Fatalf("Error getting item from database: %v", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(project)
+}
+
 func createRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", index).Methods("GET")
 	router.HandleFunc("/projects", getProjects).Methods("GET")
+	router.HandleFunc("/project/{id}", getProject).Methods("GET")
 
 	return router
 }
